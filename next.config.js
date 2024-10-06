ccab77368567efdb9de3e1661c6dcb5581daed1e
asta-maxx <tjallenmatthew@gmail.com>
2024-10-06 20:09:01 +0530

    second change on next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,35 @@
 /**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.js");
+
+import WithPWA from "next-pwa";
+
+/**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-  output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
- 
-module.exports = nextConfig
+const withPWA = WithPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development", // Disable in development mode
+  register: true,
+  scope: "/",
+  sw: "service-worker.js",
+});
+
+// @ts-ignore
+const config = withPWA({
+  reactStrictMode: true,
+
+  /**
+   * If you are using `appDir`, comment out the below `i18n` config.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+});
+
+export default config;
