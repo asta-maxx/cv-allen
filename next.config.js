9bf6ca2b8e50752de46942586667748cb1b55423
asta-maxx <tjallenmatthew@gmail.com>
2024-10-06 20:07:12 +0530

    changes on config - next

--- next.config.js
+++ next.config.js
@@ -1,31 +1,17 @@
 /**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-await import("./src/env.js");
-
-import WithPWA from "next-pwa";
-
-/**
  * @type {import('next').NextConfig}
  */
-const withPWA = WithPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development", // Disable in development mode
-  register: true,
-  scope: "/",
-  sw: "service-worker.js",
-});
-
-// @ts-ignore
-const config = withPWA({
-  reactStrictMode: true,
-  output: 'export', // Set output to export
-  distDir:'dist',
-  images: {
-    unoptimized: true, // Disable image optimization
-  },
-});
-
-// Export the config
-export default config;
+const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
